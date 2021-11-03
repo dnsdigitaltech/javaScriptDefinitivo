63a367a996aa9faf301d6f3d72982bc5d366d2f0
Davi Bernardo <dnssites@gmail.com>
2021-11-03 16:06:32 -0300

    Variáveis (2/2)

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-//codicionais, o código será executado de acordo com um condição específica
-var hora = 18;
+//definições de variaveis diferentes a partir de 2015 com o ECMAScript
+var nome = "Davi" //enviada em HOINSTING, ou seja no escopo geral da sua tela, consegue usar essa variavel, inclusiver com var global
+console.log(window.nome) //pode usar como window.nome, pois foi transferida para o escopo geral do site, podendo ser acessar em qualquer lugar
 
-if(hora < 12){
-    console.log("Bom dia")
-}else if(hora < 18){
-    console.log("Boa tarde")
-}else{
-    console.log("Boa noite")
+//usando o let
+let nome2 = "Davi" //essa variavel let fica disponivel apenas no escopo do código, apenas na area especifica do código que fica disponível, se tentar acessar de outro local não fica disponível, muito menos com o windo
+console.log(window.nome2)
+
+//outro exemplo com condicional
+var nome3 = "Davi"
+
+if(nome3 == "Davi"){
+    var idade = 90;
 }
 
-//Fazendo duas confições
-if(hora == 12 || hora == 18){
-    console.log("Você está na hora do rush")
+console.log(idade)
+
+/*if(nome3 == "Davi"){
+    let idade2 = 90;//variavel não definida, pois so funciona dentro da caixa do código
 }
+console.log(idade2)*/
+
+if(nome3 == "Davi"){
+    let idade3 = 90;//este funciona pois esta na mesma caixa
+    console.log(idade3)
+}
+
+//const é uma variavel de valor constante, uma vez que recebeu um valor não pode ser redefinido
+const nome4 = "Davi"
+nome4 = "Davi2"
+
+console.log(nome4)
