cc10503d75016b2fd1db31b7cfc3400c470aa937
Davi Bernardo <dnssites@gmail.com>
2021-11-03 14:08:34 -0300

    Condicionais (2/2)

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //codicionais, o código será executado de acordo com um condição específica
-var hora = 13;
+var hora = 18;
 
 if(hora < 12){
     console.log("Bom dia")
+}else if(hora < 18){
+    console.log("Boa tarde")
 }else{
-    console.log("Não é de manhã")
+    console.log("Boa noite")
+}
+
+//Fazendo duas confições
+if(hora == 12 || hora == 18){
+    console.log("Você está na hora do rush")
 }
