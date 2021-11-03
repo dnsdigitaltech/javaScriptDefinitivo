1c441f5c46a6fc351f1c236e1b797fdeb2b60cd9
Davi Bernardo <dnssites@gmail.com>
2021-11-03 13:43:02 -0300

    Formas de mostrar (Output)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Opa, estou fincionando!")
+//4 formas principoais para mostrar dados pro usuário final
+//1ºformula id
+document.getElementById("titulo").innerHTML = "Opa, tudo bem?" //alterou o texto
+
+//2º formula write
+document.write("Algum texto")
+
+//3º formula utilizando a janela do navegador window
+window.alert("Mansagem de exemplo")
+alert("Mansagem de exemplo")
+
+//4ª fórmula printando algo no console
+console.log("A mensagem que eu inserir aqui aparecerá no console")
